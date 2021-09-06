e378039a3026fd15f287a18a271a14a6c4364fff
M. Reza Nasirloo <m.reza.nasirloo@gmail.com>
2021-09-06 19:57:04 +0430

    Init CoolTV project

---
+++ app/src/main/java/com/tv/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.tv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.tv.ui.theme.CoolTVTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoolTVTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CoolTVTheme {
+        Greeting("Android")
+    }
+}
