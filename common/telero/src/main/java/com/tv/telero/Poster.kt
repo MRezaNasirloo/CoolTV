58fb06396d07177fb772e47da549fa72df7dde69
M. Reza Nasirloo <m.reza.nasirloo@gmail.com>
2021-09-12 17:20:36 +0430

    Improves poster design

--- common/telero/src/main/java/com/tv/telero/Poster.kt
+++ common/telero/src/main/java/com/tv/telero/Poster.kt
@@ -6,18 +6,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -25,43 +31,49 @@ import androidx.compose.ui.unit.dp
 fun Poster(
     modifier: Modifier = Modifier,
     contentDescription: String? = null,
+    title: String,
     painter: @Composable () -> Painter,
 ) {
     val context = LocalContext.current
     Column(
         modifier = modifier
             .width(128.dp)
-            .clickable {
-                Toast
-                    .makeText(context, "Clicked", Toast.LENGTH_SHORT)
-                    .show()
-            }
+            .background(MaterialTheme.colors.surface)
     ) {
-        Image(
-            modifier = Modifier
-                .aspectRatio(3 / 4f)
-                .clip(RoundedCornerShape(4.dp))
-                .background(MaterialTheme.colors.secondary),
-            painter = painter(),
-            contentDescription = contentDescription,
-        )
+        Card(
+            modifier = modifier
+                .clip(RoundedCornerShape(8.dp))
+                .aspectRatio(2 / 3f)
+                .clickable {
+                    Toast
+                        .makeText(context, "Clicked", Toast.LENGTH_SHORT)
+                        .show()
+                }
+        ) {
+            Image(
+                modifier = Modifier,
+                painter = painter(),
+                contentScale = ContentScale.Crop,
+                contentDescription = contentDescription,
+            )
+        }
         Text(
-            modifier = modifier,
-            text = "Cur genetrix experimentum!",
+            modifier = modifier
+                .fillMaxWidth()
+                .heightIn(48.dp)
+                .padding(4.dp),
+            textAlign = TextAlign.Center,
+            text = title,
             maxLines = 2,
             overflow = TextOverflow.Ellipsis
         )
     }
 }
 
-@Preview(
-    showBackground = true,
-    device = Devices.PIXEL_4,
-    showSystemUi = true
-)
+@Preview
 @Composable
 fun PosterPreview() {
-    Poster {
-        painterResource(id = R.drawable.ic_android)
+    Poster(title = "Jungle Cruise") {
+        ColorPainter(colorResource(id = R.color.teal_700))
     }
 }
