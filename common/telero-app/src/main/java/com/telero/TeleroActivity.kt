2be51ed57a3ccd78b0f9a02de5d11046727c5e45
M. Reza Nasirloo <m.reza.nasirloo@gmail.com>
2021-09-22 09:28:48 +0330

    Adds BottomTabs.kt to telero

--- common/telero-app/src/main/java/com/telero/TeleroActivity.kt
+++ common/telero-app/src/main/java/com/telero/TeleroActivity.kt
@@ -4,28 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.tv.telero.BottomTabs
 import com.tv.telero.Poster
-import com.tv.telero.theme.CoolTVTheme
+import com.tv.telero.Tab
+import com.tv.telero.theme.CoolTvTheme
 
 class TeleroActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CoolTVTheme {
+            CoolTvTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting()
+                    PosterRow()
                 }
             }
         }
@@ -33,7 +39,7 @@ class TeleroActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
+fun PosterRow() {
     val state = rememberScrollState()
     Row(
         modifier = Modifier
@@ -63,9 +69,29 @@ fun Greeting() {
 @Preview(showBackground = true, device = Devices.PIXEL_4, showSystemUi = true)
 @Composable
 fun PosterPreview() {
-    CoolTVTheme {
+    CoolTvTheme {
         Surface(color = MaterialTheme.colors.background) {
-            Greeting()
+            PosterRow()
+        }
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4, showSystemUi = true)
+@Composable
+fun PreviewBottomTabs() {
+    val tabs = listOf(
+        Tab(title = "Home", icon = R.drawable.ic_home_24),
+        Tab(title = "Profile", icon = R.drawable.ic_person_24),
+        Tab(title = "Setting", icon = R.drawable.ic_settings_24),
+    )
+    CoolTvTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            Box(
+                modifier = Modifier.fillMaxHeight(),
+                contentAlignment = Alignment.BottomCenter,
+            ) {
+                BottomTabs(modifier = Modifier.requiredHeight(56.dp), tabs)
+            }
         }
     }
 }
--- common/telero/src/main/java/com/tv/telero/theme/Theme.kt
+++ common/telero/src/main/java/com/tv/telero/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CoolTVTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun CoolTvTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
