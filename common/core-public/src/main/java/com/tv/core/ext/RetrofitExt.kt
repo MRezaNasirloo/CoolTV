b090578df324046ea5f3cb205a3c25fbd79c120d
M. Reza Nasirloo <m.reza.nasirloo@gmail.com>
2021-10-06 09:14:50 +0330

    Adds Result.kt and RetrofitExt.kt to core-public module

---
+++ common/core-public/src/main/java/com/tv/core/ext/RetrofitExt.kt
@@ -0,0 +1,18 @@
+package com.tv.core.ext
+
+import retrofit2.HttpException
+import retrofit2.Response
+
+fun <T> Response<T>.toResult(): Result<T> {
+    return try {
+        if (isSuccessful) {
+            Result.Success(requireNotNull(body()))
+        } else {
+            Result.Error(HttpException(this))
+        }
+    } catch (e: Exception) {
+        Result.Error(e)
+    }
+}
+
+fun <T> Response<T>.toResult(result: Result<T>.() -> Unit) = toResult().invoke(result)
