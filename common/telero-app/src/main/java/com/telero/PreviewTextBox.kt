716cc57838be1252550a0387ed72a31f827c9f84
M. Reza Nasirloo <m.reza.nasirloo@gmail.com>
2021-10-11 18:59:31 +0330

    Adds TextBox.kt component

---
+++ common/telero-app/src/main/java/com/telero/PreviewTextBox.kt
@@ -0,0 +1,11 @@
+package com.telero
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.tv.telero.TextBox
+
+@Preview
+@Composable
+fun PreviewTextBox() {
+    TextBox(text = "Sunt devatioes consumere varius, clemens lactaes.")
+}
