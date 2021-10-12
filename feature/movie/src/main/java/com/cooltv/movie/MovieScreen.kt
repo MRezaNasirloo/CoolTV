9ff3d2dc0eb06bb27ee5ab4166c4a80a67f9af4e
M. Reza Nasirloo <m.reza.nasirloo@gmail.com>
2021-10-12 16:34:31 +0330

    Adds progressbar to MovieScreen.kt

--- feature/movie/src/main/java/com/cooltv/movie/MovieScreen.kt
+++ feature/movie/src/main/java/com/cooltv/movie/MovieScreen.kt
@@ -1,62 +1,64 @@
 package com.cooltv.movie
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
-import com.tv.navigation.movie.entity.Ids
 import com.tv.telero.Backdrop
+import com.tv.telero.ChipGroup
 import com.tv.telero.MovieTitle
-import com.tv.telero.Poster
 import com.tv.telero.util.LogCompositions
 
 @Composable
 @Suppress("MagicNumber")
-fun MovieScreen(ids: Ids, viewModel: MovieViewModel) {
-    LogCompositions("MovieScreen")
-    val movieState = viewModel.movie.collectAsState().value
-    if (movieState == null) {
-        Text("ids: $ids \n\n loading...")
-    } else {
-        val rememberImagePainter = rememberImagePainter(
-            data = movieState.posterPath?.let {
-                "https://image.tmdb.org/t/p/w500/$it"
-            },
-            builder = {
-                crossfade(true)
-            }
-        )
-        Column(
-            Modifier.verticalScroll(rememberScrollState())
-        ) {
-            Backdrop {
-                rememberImagePainter(
-                    data = movieState.backdropPath?.let {
-                        "https://image.tmdb.org/t/p/w500/$it"
-                    },
-                    builder = {
-                        crossfade(true)
-                    }
+fun MovieScreen(viewModel: MovieViewModel) {
+    LogCompositions("MovieScreen $viewModel")
+    Crossfade(targetState = viewModel.movie.collectAsState().value) { movie ->
+        if (movie == null) {
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator(
+                    strokeWidth = 1.dp,
+                    color = MaterialTheme.colors.onSurface
                 )
             }
-            Poster {
-                rememberImagePainter
+        } else {
+            Column(
+                Modifier.verticalScroll(rememberScrollState())
+            ) {
+                Backdrop {
+                    rememberImagePainter(
+                        data = movie.backdropPath?.let {
+                            "https://image.tmdb.org/t/p/w500/$it"
+                        },
+                        builder = {
+                            crossfade(true)
+                        }
+                    )
+                }
+                MovieTitle(
+                    title = movie.title,
+                    year = movie.releaseDate?.substring(0, 4)?.toInt(),
+                    certification = null,
+                    runtime = movie.runtime
+                )
+                ChipGroup(chips = movie.genres.map { it.name })
+                Text(text = movie.status)
+                Text(text = movie.overview ?: "")
+                Text(text = movie.voteAverage.toString())
+                Text(text = movie.voteCount.toString())
+                Text(text = movie.popularity.toString())
             }
-            MovieTitle(
-                title = movieState.title,
-                year = movieState.releaseDate?.substring(0, 4)?.toInt(),
-                certification = null,
-                runtime = movieState.runtime
-            )
-            Text(text = movieState.status)
-            Text(text = movieState.overview ?: "")
-            Text(text = movieState.voteAverage.toString())
-            Text(text = movieState.voteCount.toString())
-            Text(text = movieState.popularity.toString())
         }
     }
 }
