db02e5bb532c60868fbf81bca5e876c8c75c4dc6
M. Reza Nasirloo <m.reza.nasirloo@gmail.com>
2021-10-13 08:56:46 +0330

    Adds network cache

--- common/core-wiring/src/main/java/com/tv/core/di/CoreModule.kt
+++ common/core-wiring/src/main/java/com/tv/core/di/CoreModule.kt
@@ -4,18 +4,23 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.tv.core.task.LoggerTask
 import com.tv.core.task.StartupTask
+import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.File
 
 internal val CoreModule = module {
     factory { LoggerTask() } bind StartupTask::class
 
     factory {
         OkHttpClient.Builder().apply {
+            @Suppress("MagicNumber")
+            cache(Cache(File(androidContext().cacheDir, "okhttp_cache"), 1024 * 5L /* 5MB */))
             getAll<Interceptor>()
                 .distinctBy { it::class }
                 .forEach { addInterceptor(it) }
