5f47f987de97a4f56b50162bc7e941e63716e0ac
M. Reza Nasirloo <m.reza.nasirloo@gmail.com>
2021-10-22 14:31:01 +0330

    Fixing -> Adds animation to TrendingScreen.kt

--- feature/trending/src/main/java/com/tv/trending/TrendingScreen.kt
+++ feature/trending/src/main/java/com/tv/trending/TrendingScreen.kt
@@ -1,10 +1,14 @@
 package com.tv.trending
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
@@ -12,6 +16,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -20,6 +25,7 @@ import com.tv.telero.CircularLoading
 import com.tv.telero.image.Poster
 import com.tv.telero.text.TextBox
 import com.tv.telero.text.Title
+import com.tv.telero.util.LogCompositions
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import org.koin.androidx.compose.getViewModel
@@ -33,29 +39,44 @@ fun TrendingScreen(
     onPosterClick: (Ids) -> Unit
 ) {
     val trendingState by viewModel.trending.collectAsState()
-    AnimatedContent(targetState = trendingState) { state ->
+    AnimatedContent(targetState = trendingState,
+    transitionSpec = {
+        if (initialState !is TrendingViewModel.State.Success && targetState is TrendingViewModel.State.Success) {
+            // If the target number is larger, it slides up and fades in
+            // while the initial (smaller) number slides up and fades out.
+            slideInVertically({ height -> height }) + fadeIn() with
+                    slideOutVertically({ height -> -height }) + fadeOut()
+        } else {
+            // If the target number is smaller, it slides down and fades in
+            // while the initial number slides down and fades out.
+            EnterTransition.None with ExitTransition.None
+        }
+    }
+    ) { state ->
         when (state) {
             is TrendingViewModel.State.Loading -> CircularLoading()
             is TrendingViewModel.State.Error -> TextBox(text = "Something went wrong!")
             is TrendingViewModel.State.Success -> {
-                Column(
-                    modifier = Modifier.animateEnterExit(
-                        enter = slideInVertically(
-                            initialOffsetY = { it / SLIDE_OFFSET_MULTIPLAYER },
-                            animationSpec = tween()
-                        ) + fadeIn(
-                            animationSpec = tween()
-                        )
-                    )
-                ) {
-                    Title(title = "Trending")
-                    LazyRow(Modifier.padding(horizontal = 8.dp)) {
-                        items(state.items) { movie ->
-                            PosterDetail(Modifier.padding(2.dp), movie) {
-                                onPosterClick(movie.ids)
+                key("column") {
+                    Column(
+                        modifier = Modifier
+                    ) {
+                        LogCompositions(tag = "Column")
+                        key("Trending") {
+                            Title(title = "Trending")
+                        }
+                        key("lazyRow") {
+                            LogCompositions("LazyRow")
+                            LazyRow(Modifier.padding(horizontal = 8.dp)) {
+                                items(state.items) { movie ->
+                                    PosterDetail(Modifier.padding(2.dp), movie) {
+                                        onPosterClick(movie.ids)
+                                    }
+                                }
                             }
                         }
                     }
+
                 }
             }
         }
@@ -70,6 +91,7 @@ fun PosterDetail(
     movie: TrendingViewModel.Movie,
     click: () -> Unit
 ) {
+    LogCompositions("PosterDetail ${movie.title}")
     val painter = rememberImagePainter(
         data = movie.poster?.posterPath,
     )
